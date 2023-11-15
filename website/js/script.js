148fba3654a1ea8dc87827140a71eaeded04d210
owenavon <89999663+owenavon@users.noreply.github.com>
2023-11-15 12:37:05 -0500

    Worked on Contrast section, went through WCAG about contrast, fixed animation, added text on main section and banner on index page.

--- website/js/script.js
+++ website/js/script.js
@@ -1,7 +1,7 @@
 
 //Colour Picker
 document.getElementById("colorpicker").addEventListener("change", function() {
-    document.getElementById("sec2").style.backgroundColor = this.value;
+    document.getElementById("header").style.backgroundColor = this.value;
 });
 
 //Colour Picker
@@ -13,20 +13,11 @@ $(document).ready(function() {
     $("body").tooltip({ selector: '[data-toggle=tooltip]' });
 });
 
-
-//Animation
-//function chageWord() {
-//  let word = document.getElementsByTagName("span");
-//  console.log(word);
-//  let words = ["extra", "super", "really", "extra", "so"];
-//  word[0].innerHTML = words[Math.floor(Math.random() * words.length)];
-//}
-
 function run() {
   let timer;
   timer = setInterval(function () {
     chageWord();
-  }, 4000);
+  }, 2000);
 }
 
 run();
